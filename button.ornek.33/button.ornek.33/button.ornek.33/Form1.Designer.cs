eabecec41cc765620a2a086c6dac209dd9310d1f
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-23 13:06:57 +0300

    Add files via upload

---
+++ button.ornek.33/button.ornek.33/button.ornek.33/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace button.ornek._33
+{
+    partial class txtBitis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.Sayilar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Başlangıç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bitiş";
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Location = new System.Drawing.Point(108, 27);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(100, 20);
+            this.txtBaslangic.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnYaz
+            // 
+            this.btnYaz.Location = new System.Drawing.Point(108, 132);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(75, 65);
+            this.btnYaz.TabIndex = 4;
+            this.btnYaz.Text = "Yaz";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // Sayilar
+            // 
+            this.Sayilar.FormattingEnabled = true;
+            this.Sayilar.Location = new System.Drawing.Point(299, 60);
+            this.Sayilar.Name = "Sayilar";
+            this.Sayilar.Size = new System.Drawing.Size(120, 147);
+            this.Sayilar.TabIndex = 5;
+            // 
+            // txtBitis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 366);
+            this.Controls.Add(this.Sayilar);
+            this.Controls.Add(this.btnYaz);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtBaslangic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "txtBitis";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnYaz;
+        private System.Windows.Forms.ListBox Sayilar;
+    }
+}
+
---
+++ button.ornek.33/button.ornek.33/button.ornek.33/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._33
+{
+    public partial class txtBitis : Form
+    {
+        public txtBitis()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            Sayilar.Items.Clear();
+            int baslangic = Convert.ToInt32(txtBaslangic.Text);
+            int bitis = Convert.ToInt32(textBox2.Text);
+
+            for (int i = baslangic; i < bitis; i++)
+            {
+                Sayilar.Items.Add(i);
+            }
+            
+                
+            
+
+        }
+    }
+}
